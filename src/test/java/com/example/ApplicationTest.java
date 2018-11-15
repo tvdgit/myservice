f7d3f9682b5a9eccbef72a6f9093fcbb5195ef73
Marco Facetti <marco.facetti@trviadis.com>
2018-11-15 15:39:38 +0100

    .

---
+++ src/test/java/com/example/ApplicationTest.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+/**
+ * @author Marco Facetti
+ */
+class ApplicationTest {
+
+  @BeforeEach
+  void setUp() {
+    assertEquals(1, 1);
+  }
+}
